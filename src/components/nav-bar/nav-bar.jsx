3d5b36e1d620e846e474bd4a80bdd2f764df6655
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-08-28 23:24:49 -0400

    nav fix

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -25,18 +25,18 @@ export function NavBar({user}) {
     <Navbar className = "main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
       <Container>
         <Navbar.Brand className="navbar-logo">
-        <Link href="/">myFlix</Link></Navbar.Brand>
+        <Link to="/">myFlix</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             {isAuth() && (
-              <Link className="user" href={`/users/${user?.Username}`}>{user?.Username}</Link>
+              <Link className="user" to={`/users/${user?.Username}`}>{user?.Username}</Link>
             )}
             {isAuth() && (
               <Button variant="link" onClick={() => { onLoggedOut() }}>Logout</Button>
             )}
-            {!isAuth() && (<Link href="/">Sign In</Link>)}
-            {!isAuth() && (<Link href="/register">Register</Link>)}
+            {!isAuth() && (<Link to="/">Sign In</Link>)}
+            {!isAuth() && (<Link to="/register">Register</Link>)}
           </Nav>
         </Navbar.Collapse>
       </Container>
