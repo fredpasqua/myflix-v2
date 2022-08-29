f1e42b01e4d8c646d91544ecbe9bbc675a40a0f8
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-08-28 23:21:17 -0400

    navbar fixes

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,7 +1,7 @@
 import React from  'react';
 import './nav-bar.scss'
 import { Navbar, Nav, Container, Button } from 'react-bootstrap'
-
+import { Link } from "react-router-dom"
 
 export function NavBar({user}) {
 
@@ -24,19 +24,19 @@ export function NavBar({user}) {
   return (
     <Navbar className = "main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand className="navbar-logo" 
-        href="/">myFlix</Navbar.Brand>
+        <Navbar.Brand className="navbar-logo">
+        <Link href="/">myFlix</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             {isAuth() && (
-              <Nav.Link className="user" href={`/users/${user?.Username}`}>{user?.Username}</Nav.Link>
+              <Link className="user" href={`/users/${user?.Username}`}>{user?.Username}</Link>
             )}
             {isAuth() && (
               <Button variant="link" onClick={() => { onLoggedOut() }}>Logout</Button>
             )}
-            {!isAuth() && (<Nav.Link href="/">Sign In</Nav.Link>)}
-            {!isAuth() && (<Nav.Link href="/register">Register</Nav.Link>)}
+            {!isAuth() && (<Link href="/">Sign In</Link>)}
+            {!isAuth() && (<Link href="/register">Register</Link>)}
           </Nav>
         </Navbar.Collapse>
       </Container>
