1e01c32c20f548c729aa4e494c12ce0ce0fa623a
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-10-17 17:29:19 -0400

    formating

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,55 +1,61 @@
-import React from 'react'
-import './director-view.scss'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import { CardGroup, Card} from 'react-bootstrap';
+import React from "react";
+import "./director-view.scss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { CardGroup, Card } from "react-bootstrap";
 
 export class DirectorView extends React.Component {
+  render() {
+    const { director, onBackClick, movies } = this.props;
+    console.log({ movies });
 
-    render() {
-        const { director, onBackClick, movies } = this.props;
-        console.log({movies});
-
-        if(movies)
-    return (
-       
-    <>
-    <Row> 
-        <Col><h1 className='page_title'>Director Info</h1></Col>
-    </Row>
-
-    <Row>
-        <Col>  <h2>{director.Name}</h2>
-                 <p>Born in {director.Birth}</p>
-                 <p className='title'>{director.Bio}</p>
-                
-        </Col>
-    </Row> 
-    <Row> 
-        <Col><h2 className='page_title2'>Movies by {director.Name}</h2></Col>
-    </Row>
-    <Row>
-         <Col md={12}>
-                <CardGroup className="dirMovies">
-                    {movies.map(movie => (
-                     
-                            <Card.Img
-                                className="fav-poster"
-                                variant="top"
-                                src={movie.ImagePath} />
-                       
-                            ))}
-                </CardGroup>
+    if (movies)
+      return (
+        <>
+          <Row>
+            <Col>
+              <h1 className="page_title">Director Info</h1>
             </Col>
-    </Row>
-    <Row className="button">
-    <Button  variant="info" onClick={() => { onBackClick(); }}>Back</Button>
-    </Row>
-    </>
-        )
-    }
-    
+          </Row>
 
+          <Row>
+            <Col>
+              {" "}
+              <h2>{director.Name}</h2>
+              <p>Born in {director.Birth}</p>
+              <p className="title">{director.Bio}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2 className="page_title2">Movies by {director.Name}</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <CardGroup className="dirMovies">
+                {movies.map((movie) => (
+                  <Card.Img
+                    className="fav-poster"
+                    variant="top"
+                    src={movie.ImagePath}
+                  />
+                ))}
+              </CardGroup>
+            </Col>
+          </Row>
+          <Row className="button">
+            <Button
+              variant="info"
+              onClick={() => {
+                onBackClick();
+              }}
+            >
+              Back
+            </Button>
+          </Row>
+        </>
+      );
+  }
 }
-  
--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,55 +1,61 @@
-import React from 'react';
-
-import './genre-view.scss'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-
-import icon from './masks.png'
-import { CardGroup, Card} from 'react-bootstrap';
+import React from "react";
+import "./genre-view.scss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import icon from "./masks.png";
+import { CardGroup, Card } from "react-bootstrap";
 
 export class GenreView extends React.Component {
+  render() {
+    const { genre, onBackClick, movies } = this.props;
 
-    render() {
-        const { genre, onBackClick, movies } = this.props;
-    
     return (
-       
-    <>
-            <Row className="justify-content-center">
-                 <Col> <div><img className='image' src={icon} alt='icon' /></div></Col> 
-                 <Col>  <h2>{genre.Name}</h2>
-                 <p>{genre.Description}</p>
-                 </Col>
-                 </Row>   
-                
-               
- 
+      <>
+        <Row className="justify-content-center">
+          <Col>
+            {" "}
+            <div>
+              <img className="image" src={icon} alt="icon" />
+            </div>
+          </Col>
+          <Col>
+            {" "}
+            <h2>{genre.Name}</h2>
+            <p>{genre.Description}</p>
+          </Col>
+        </Row>
 
-    <Row> 
-        <Col><p className='page_title2'>{genre.Name} Movies</p></Col>
-    </Row>
-    <Row>
-         <Col md={12}>
-                <CardGroup className="genreMovies">
-                    {movies.map(movie => (
-                     
-                            <Card.Img 
-                                className="fav-poster"
-                                variant="top"
-                                src={movie.ImagePath} />
-                       
-                            ))}
-                </CardGroup>
-            </Col>
-    </Row>
+        <Row>
+          <Col>
+            <p className="page_title2">{genre.Name} Movies</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <CardGroup className="genreMovies">
+              {movies.map((movie) => (
+                <Card.Img
+                  className="fav-poster"
+                  variant="top"
+                  src={movie.ImagePath}
+                />
+              ))}
+            </CardGroup>
+          </Col>
+        </Row>
 
-    
-    <Row className="button">
-    <Button  variant="info" onClick={() => { onBackClick(); }}>Back</Button>
-    </Row>         
-    </>
-        )
-    }
-    
+        <Row className="button">
+          <Button
+            variant="info"
+            onClick={() => {
+              onBackClick();
+            }}
+          >
+            Back
+          </Button>
+        </Row>
+      </>
+    );
+  }
 }
